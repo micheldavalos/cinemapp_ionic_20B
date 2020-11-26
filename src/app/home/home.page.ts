8e7596e45b478226b684213f9f867000c3e5397e
Michel Dávalos <michel.davalos@academicos.udg.mx>
2020-11-26 07:40:40 -0600

    Mostrar peliculas en un ion-card Crear Servicio intermediario para la conexión http a la RESTFul API de Flask Subject para generar un suscriber para la lista de peliculas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeliculasService } from '../api/peliculas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  peliculas = []; /* [
+    {
+      titulo: "Matrix",
+      anio: 1999,
+      director: "Hermanas Wachosky",
+      imagen: "https://i.ytimg.com/vi/7L6BheyqeaM/maxresdefault.jpg"
 
-  constructor() {}
+    },
+    {
+      titulo: "28 Days Later",
+      anio: 2003,
+      director: "Danny Boyle",
+      imagen: "https://reeltimedublin.files.wordpress.com/2015/10/28-days-later-2002-poster.jpg"
+
+    }
+  ];*/
+
+  constructor(private peliculasService: PeliculasService) {}
+
+  ngOnInit() {
+    this.peliculasService.peliculas.subscribe(peliculas => {
+      this.peliculas = peliculas;
+    });
+    // this.peliculasService.getPeliculas();
+  }
 
 }
